34abf3eb2fb783c030b8a3d8d188a0ef27ae18ef
Daniel Liu <danielliu@berkeley.edu>
2026-02-14 22:25:21 -0800

    add quotes

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,28 @@
 
 export const SITE_TITLE = 'daniel\'s website';
 export const SITE_DESCRIPTION = 'daniel\'s personal website';
+
+export const QUOTES = [
+    {
+        text: [
+          "No mind to think.",
+          "No will to break.",
+          "No voice to cry suffering."
+        ],
+        author: "Hollow Knight"
+    },
+    {
+        text: [
+            "Thou crawler!",
+            "Thou cringer!",
+            "Thou smallest of the small!"
+        ],
+        author: "Hollow Knight"
+    },
+    {
+        text: [
+            "Poshanka!"
+        ],
+        author: "Hollow Knight: Silksong"
+    }
+]
